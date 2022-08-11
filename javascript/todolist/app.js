8aa1d7e3afee4a75adee3731bb27a6acfd59c52d
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-08-11 19:52:41 -0300

    list option added

---
+++ javascript/todolist/app.js
@@ -0,0 +1,9 @@
+let user = prompt('What would you like to do?')
+const todo = [];
+while (user !== 'quit') {
+    if (user === 'new'){
+        let nueva = prompt('Enter new todo');
+    }else if(user === list){
+        console.log(todo);
+    }
+}
