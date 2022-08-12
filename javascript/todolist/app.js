139b10aad8c91119e982b269c67e6eafb1775f43
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-08-12 08:25:49 -0300

    added delete option

--- javascript/todolist/app.js
+++ javascript/todolist/app.js
@@ -1,19 +1,23 @@
 let input = prompt("What would you like to do?")
 
-const todo = [];
+const todos = [];
 
-while (input !== "quit" && "q") {
-    input = prompt("What would you like to do?")
+while (input !== "quit" && input !=="q") {
     if (input === "new") {
-        let nueva = prompt("Add a new todo")
-        let tarea = todo.unshift(nueva);
-        console.log(todo);
+        const newTodo = prompt("Add a new todo")
+        todos.push(newTodo);
+        console.log(todos);
     } else if (input === "list") {
         console.log(`********`)
-        for (let i = 0; i < todo.length; i++){
-            console.log(todo[i]);
+        for (let i = 0; i < todos.length; i++){
+            console.log(`${i}: ${todos[i]}`);
         }
         console.log('********')
+    } else if (input === 'delete') {
+        const index = prompt('Ok, enter index to delete:')
+        const deleted = todos.splice(index, 1);
+        console.log(`Ok, deleted ${deleted[0]}`);
     }
+    input = prompt("What would you like to do?")
 }
 console.log("Ok, you quit the app")
