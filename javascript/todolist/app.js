a70bce7cfa36580dd70bde33b959434c7995908a
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-08-12 08:05:10 -0300

    message of list option

--- javascript/todolist/app.js
+++ javascript/todolist/app.js
@@ -1,9 +1,19 @@
-let user = prompt('What would you like to do?')
+let input = prompt("What would you like to do?")
+
 const todo = [];
-while (user !== 'quit') {
-    if (user === 'new'){
-        let nueva = prompt('Enter new todo');
-    }else if(user === list){
+
+while (input !== "quit" && "q") {
+    input = prompt("What would you like to do?")
+    if (input === "new") {
+        let nueva = prompt("Add a new todo")
+        let tarea = todo.unshift(nueva);
         console.log(todo);
+    } else if (input === "list") {
+        console.log(`********`)
+        for (let i = 0; i < todo.length; i++){
+            console.log(todo[i]);
+        }
+        console.log('********')
     }
 }
+console.log("Ok, you quit the app")
