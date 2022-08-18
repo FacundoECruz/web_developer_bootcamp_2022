2228b1a21e3bbb57fdf52756d8926e9726ed010c
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-08-18 18:39:56 -0300

    preventDefault and append exercise

---
+++ javascript/25/form/app.js
@@ -0,0 +1,15 @@
+const form = document.querySelector("#shelterForm");
+const input = document.querySelector("#filoName");
+const list = document.querySelector("#filos");
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const filoName = input.value;
+    if (filoName === 'Hobbes' || filoName === 'Locke' || filoName === 'Spencer') {
+        alert('Ese tipo no es rapero!!!!');
+        input = "";
+    }
+    const newLI = document.createElement("LI");
+    newLI.innerText = filoName;
+    list.append(newLI);
+    input.value = "";
+});
