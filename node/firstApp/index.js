f7f33375b3df70974ff72ced42b3f8d686727b12
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-08-27 14:43:49 -0300

    cats and dogs response added

--- node/firstApp/index.js
+++ node/firstApp/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res) => {
-    console.log("WE GOT A NEW REQUEST!!");
-    res.send('<h1>This is my webpage!!</h1>')
+// app.use((req, res) => {
+//     console.log("WE GOT A NEW REQUEST!!");
+//     res.send('<h1>This is my webpage!!</h1>')
+// })
+
+app.get('/', (req, res) => {
+    res.send('This is the home page!')
+})
+
+app.get('/cats', (req, res) => {
+    res.send('MEOW!!')
+})
+
+app.get('/DOGS', (req, res) => {
+    res.send('WOOF!!')
 })
 
 app.listen(3000, () => {
