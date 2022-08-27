f2050355f403579051545131fd4c3da5eb7f60dd
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-08-27 15:09:34 -0300

    postID added

--- node/firstApp/index.js
+++ node/firstApp/index.js
@@ -12,7 +12,12 @@ app.get('/', (req, res) => {
 
 app.get('/r/:subreddit', (req, res) => {
     const { subreddit } = req.params;
-    res.send(`<h1>Browsing the ${subreddit} subreddit`)
+    res.send(`<h1>Browsing the ${subreddit} subreddit</h1>`)
+})
+
+app.get('/r/:subreddit/:postID', (req, res) => {
+    const { subreddit, postID } = req.params;
+    res.send(`<h1>Viewing Post ID: ${postID} on the ${subreddit} subreddit</h1>`)
 })
 
 app.get('/cats', (req, res) => {
