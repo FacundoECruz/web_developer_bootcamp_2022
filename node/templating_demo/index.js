588ff4ac171d4fe7910a247cbeb9a584d4474a47
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-08-28 19:09:51 -0300

    stylesheet added

--- node/templating_demo/index.js
+++ node/templating_demo/index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const redditData = require('./data.json');
 
-
+app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
 
@@ -21,8 +21,12 @@ app.get('/cats', (req, res) => {
 app.get('/r/:subreddit', (req, res) => {
     const { subreddit } = req.params;
     const data = redditData[subreddit];
-    console.log(data)
-    res.render('subreddit', { ...data });
+    if (data) {
+        res.render('subreddit', { ...data });
+    } else {
+        res.render('notfound', { subreddit })
+    }
+
 })
 
 app.get('/rand', (req, res) => {
