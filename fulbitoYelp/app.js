e304e7f5e6aa28e0198859333a71bef840ecedfa
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-10-13 10:27:24 -0300

    db connection

--- fulbitoYelp/app.js
+++ fulbitoYelp/app.js
@@ -2,7 +2,16 @@ const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/YelpSoccer');
+mongoose.connect('mongodb://localhost:27017/YelpSoccer', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected")
+})
 
 const app = express()
 
