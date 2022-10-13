042d8c375f126296eed654c80341dd4776f374c1
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-10-13 16:59:17 -0300

    seed db with model's location

--- fulbitoYelp/app.js
+++ fulbitoYelp/app.js
@@ -20,7 +20,7 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', async (req, res) => {
-    const field = new SoccerField({title: 'Futbol 5 LP', description: 'Alta canchita'});
+    const field = new SoccerField({title: 'Fulwacho', description: 'Futbol champagne'});
     await field.save();
     res.send(field);
 })
--- fulbitoYelp/seeds/index.js
+++ fulbitoYelp/seeds/index.js
@@ -1,10 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const SoccerField = require('../models/soccerField')
+const cities = require('./cities');
 
-mongoose.connect('mongodb://localhost:27017/YelpSoccer', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+mongoose.connect('mongodb://localhost:27017/YelpSoccer', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("CONECTED TO MONGO")
+    })
+    .catch(err => {
+        console.log('ERROR')
+        console.log(err)
+    })
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -12,10 +17,21 @@ db.once("open", () => {
     console.log("Database connected")
 })
 
-const seedDB = async () => {
-    await SoccerField.deleteMany({});
-    const s = new SoccerField({ title: 'Cancha' });
-    await s.save();
-}
 
-seedDB;
+//const seedDB = () => {
+    for (let i = 0; i < 20; i++){
+        const random24 = Math.floor(Math.random() * 24);
+        const field = new SoccerField({
+            location: `${cities[random24].nombre_completo}`
+        })
+        field.save()
+        .then(field => {
+            console.log(field)
+        })
+        .catch(e => {
+            console.log(e)
+        })
+    }
+//}
+
+//seedDB;
