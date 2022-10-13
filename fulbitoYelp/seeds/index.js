1c72d05ef2c523bc0abe9fdcdd8b26f8d90c8d84
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-10-13 17:09:08 -0300

    db seeded now with titles too

--- fulbitoYelp/seeds/index.js
+++ fulbitoYelp/seeds/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const SoccerField = require('../models/soccerField')
 const cities = require('./cities');
+const { names } = require('./seedHelpers')
 
 mongoose.connect('mongodb://localhost:27017/YelpSoccer', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
@@ -18,20 +19,20 @@ db.once("open", () => {
 })
 
 
-//const seedDB = () => {
-    for (let i = 0; i < 20; i++){
-        const random24 = Math.floor(Math.random() * 24);
-        const field = new SoccerField({
-            location: `${cities[random24].nombre_completo}`
-        })
-        field.save()
+for (let i = 0; i < 20; i++) {
+    const random24 = Math.floor(Math.random() * 24);
+    const field = new SoccerField({
+        title: `${names[i]}`,
+        location: `${cities[random24].nombre_completo}`
+    })
+    field.save()
         .then(field => {
             console.log(field)
         })
         .catch(e => {
             console.log(e)
         })
-    }
-//}
+}
 
-//seedDB;
+
+
---
+++ fulbitoYelp/seeds/seedHelpers.js
@@ -0,0 +1,22 @@
+module.exports.names = [
+    'Tierra de campeones',
+    'El estadio',
+    'La caldera',
+    'Bernabeu',
+    'Bombonera',
+    'Monumental',
+    'Mestalla',
+    'Camp nou',
+    'Potrero',
+    'Qatar',
+    'Megaestadio',
+    'Supercancha',
+    'VamoarribaUruguay',
+    'Bosque',
+    'Uno',
+    'Santos',
+    'Old Trafford',
+    'Highbury',
+    'Rio Grande',
+    'Ensenada'
+]
