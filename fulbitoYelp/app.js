a2e97a12db7733cec72374aac24d4ce390dc75c4
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-10-15 13:33:12 -0300

    put route created

--- fulbitoYelp/app.js
+++ fulbitoYelp/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
 const SoccerField = require('./models/soccerField')
+const methodOverride = require('method-override')
 
 mongoose.connect('mongodb://localhost:27017/YelpSoccer', {
     useNewUrlParser: true,
@@ -20,6 +21,7 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/', async (req, res) => {
     res.render('home')
@@ -48,6 +50,12 @@ app.get('/soccerfields/:id', async (req, res) => {
 app.get('/soccerfields/:id/edit', async (req,res) => {
     const soccerfield = await SoccerField.findById(req.params.id);
     res.render('soccerfields/edit', { soccerfield });
+})
+
+app.put('/soccerfields/:id', async (req, res) => {
+    const { id } = req.params;
+    const soccerfield = await SoccerField.findByIdAndUpdate(id, { ...req.body.soccerfield}, { new: true })
+    res.redirect(`/soccerfields/${ soccerfield.id }`)
 })
 
 app.listen(3000, () => {
