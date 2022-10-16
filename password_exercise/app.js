78aa0db36d31dfa7dd40aad7e6669e1444bf91ba
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-10-16 13:00:30 -0300

    print req.method middleware

--- password_exercise/app.js
+++ password_exercise/app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 
+ 
+app.use(express.urlencoded({ extended:true })); 
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.method);
+    next(); 
+})
+
 app.get('/', (req, res) => {
     res.send('HOME PAGE')
 })
