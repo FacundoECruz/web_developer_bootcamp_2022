e9e0b80e429092b4b98acb5687078a2f0451d997
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-10-17 06:45:10 -0300

    images and descriptions added to the show page

--- fulbitoYelp/seeds/index.js
+++ fulbitoYelp/seeds/index.js
@@ -21,9 +21,13 @@ db.once("open", () => {
 
 for (let i = 0; i < 20; i++) {
     const random24 = Math.floor(Math.random() * 24);
+    const price = Math.floor(Math.random() * 2000) + 10;
     const field = new SoccerField({
         title: `${names[i]}`,
-        location: `${cities[random24].nombre_completo}`
+        location: `${cities[random24].nombre_completo}`,
+        image: 'https://imgar.zonapropcdn.com/avisos/1/00/44/29/27/76/1200x1200/1724306414.jpg',
+        description: 'Cancha de futbol 5 para disfrutar con amigues, despues te escabias y te jugas un altisima, en breve el anotador online.',
+        price
     })
     field.save()
         .then(field => {
