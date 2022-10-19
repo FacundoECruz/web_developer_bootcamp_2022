6056ee439cce92bcbe76624d0ab65ea014d8c3fe
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-10-19 14:34:13 -0300

    first middleware error handling excercise

--- password_exercise/app.js
+++ password_exercise/app.js
@@ -10,7 +10,7 @@ const verifyPassword = (req, res, next) => {
     if(password === 'gato'){
         next();
     }
-    res.send('You need a password')
+    throw new Error('You need a password')
 }
 
 // app.use((req, res, next) => {
@@ -29,6 +29,13 @@ app.get('/secret', verifyPassword, (req, res) => {
 app.use((req, res) => {
     res.status(404).send('Something broke!')
   })
+
+app.use((err, req, res, next) => {
+    console.log('************************')
+    console.log('*********ERROR**********')
+    console.log('************************')
+    res.status(500).send("Oh boooy, we've got an errorrrr!!!")
+}) 
 
 app.listen(3000, () => {
     console.log("Listen on 3000")
