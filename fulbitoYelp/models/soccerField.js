7c11dfd2b526c4445f4ef8c3265f57a8a58b81fd
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-10-26 15:43:06 -0300

    mongoose delete middleware setted

--- fulbitoYelp/models/soccerField.js
+++ fulbitoYelp/models/soccerField.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Review = require('./review')
 const Schema = mongoose.Schema
 
 const SoccerFieldSchema = new Schema({
@@ -15,5 +16,15 @@ const SoccerFieldSchema = new Schema({
     ]
 },
 { collection: 'soccerfields' })
+
+SoccerFieldSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.remove({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
 
 module.exports = mongoose.model('SoccerField', SoccerFieldSchema)
