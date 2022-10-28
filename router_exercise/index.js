4f88bbf9070ea0a36aa3fded9c552ac420bf3da2
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-10-28 19:46:59 -0300

    cinema example added

--- router_exercise/index.js
+++ router_exercise/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const teatrosRoutes = require('./routes/teatros')
+const cinesRoutes = require('./routes/cines')
 
-app.use('/', teatrosRoutes);
+app.use('/teatros', teatrosRoutes);
+app.use('/cines', cinesRoutes);
 
 app.listen(3000, () => {
     console.log("LISTEN ON 3000")
--- router_exercise/routes/cines.js
+++ router_exercise/routes/cines.js
@@ -2,19 +2,19 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.send('TODOS LOS TEATROS')
+    res.send('TODOS LOS CINES')
 })
 
 router.post('/', (req, res) => {
-    res.send('CREAR TEATRO')
+    res.send('CREAR CINE')
 })
 
 router.get('/:id', (req, res) => {
-    res.send('MOSTRAR UN TEATRO')
+    res.send('MOSTRAR UN CINE')
 })
 
 router.get('/:id/edit', (req, res) => {
-    res.send('EDITAR UN TEATRO')
+    res.send('EDITAR UN CINE')
 })
 
 module.exports = router;
