88f42ce6bae1873b702d1a240f479461d639aec0
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-10-28 19:36:37 -0300

    router file created

---
+++ router_exercise/routes/teatros.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/teatros', (req, res) => {
+    res.send('')
+})
+
+router.post('/teatros', (req, res) => {
+    res.send('')
+})
+
+router.get('/teatros', (req, res) => {
+    res.send('')
+})
+
+router.get('/teatros', (req, res) => {
+    res.send('')
+})
+
