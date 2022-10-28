7c7188b625284d65db140b42a895c8c2c9cb9a76
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-10-28 19:38:21 -0300

    router setted

--- router_exercise/routes/teatros.js
+++ router_exercise/routes/teatros.js
@@ -2,18 +2,19 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/teatros', (req, res) => {
-    res.send('')
+    res.send('TODOS LOS TEATROS')
 })
 
 router.post('/teatros', (req, res) => {
-    res.send('')
+    res.send('CREAR TEATRO')
 })
 
-router.get('/teatros', (req, res) => {
-    res.send('')
+router.get('/teatros/:id', (req, res) => {
+    res.send('MOSTRAR UN TEATRO')
 })
 
-router.get('/teatros', (req, res) => {
-    res.send('')
+router.get('/teatros/:id/edit', (req, res) => {
+    res.send('EDITAR UN TEATRO')
 })
+
 
