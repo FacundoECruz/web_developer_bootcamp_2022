8128239a850fd3cd90799d58333c281e55b16902
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-10-30 08:37:54 -0300

    middleware setted

--- router_exercise/index.js
+++ router_exercise/index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 const teatrosRoutes = require('./routes/teatros')
 const cinesRoutes = require('./routes/cines')
+const adminRoutes = require('./routes/admin')
 
 app.use('/teatros', teatrosRoutes);
 app.use('/cines', cinesRoutes);
+app.use('/admin', adminRoutes);
 
 app.listen(3000, () => {
     console.log("LISTEN ON 3000")
--- router_exercise/routes/admin.js
+++ router_exercise/routes/admin.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
+router.use((req, res, next) => {
+    if(req.query.isAdmin) {
+        next();
+    }
+    res.send("NO SOS ADMIN")
+})
+
 router.get('/secret', (req, res) => {
     res.send('ESTO ES UN SECRETOOOOO!!!!')
 })
