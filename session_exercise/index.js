a0dbf22ba26633632fa0fb2fb715640a8b07979a
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-10-31 17:23:17 -0300

    register exercise

--- session_exercise/index.js
+++ session_exercise/index.js
@@ -15,6 +15,17 @@ app.get('/viewcount', (req, res) => {
     res.send(`You have seen this page ${req.session.count} times`)
 })
 
+app.get('/register', (req, res) => {
+    const { username = 'Anonymous'} = req.query;
+    req.session.username = username;
+    res.redirect('/greet')
+})
+
+app.get('/greet', (req, res) => {
+    const { username } = req.session;
+    res.send(`Welcome back, ${username}`)
+})
+
 app.listen(3000, () => {
     console.log('LISTEN ON 3000')
 })
