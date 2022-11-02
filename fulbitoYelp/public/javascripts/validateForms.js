262d6294849b239c74997b8d6e725e7c4e124455
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-11-02 08:19:44 -0300

    path.join for public directory

--- fulbitoYelp/public/javascripts/validateForms.js
+++ fulbitoYelp/public/javascripts/validateForms.js
@@ -1,4 +1,4 @@
-(() => {
+(function () {
     'use strict'
 
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
@@ -15,4 +15,4 @@
             form.classList.add('was-validated')
         }, false)
     })
-})
+})()
