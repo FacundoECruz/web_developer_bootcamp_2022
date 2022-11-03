bf954fd6e5a83e481cc0241f706ae84b890bf01e
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-11-03 15:05:11 -0300

    password hashed

--- auth_exercises/bcrypt_exercise/index.js
+++ auth_exercises/bcrypt_exercise/index.js
@@ -1,8 +1,10 @@
 const bcrypt = require('bcrypt')
 
-const hashPassword = async () => {
+const hashPassword = async (pw) => {
     const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(pw, salt)
     console.log(salt)
+    console.log(hash)
 }
 
-hashPassword();
+hashPassword('antone');
