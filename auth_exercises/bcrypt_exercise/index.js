da2c7d177fd9d4bd6ae9a40ae398b284d34e3ee6
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-11-03 15:08:50 -0300

    login function

--- auth_exercises/bcrypt_exercise/index.js
+++ auth_exercises/bcrypt_exercise/index.js
@@ -1,10 +1,21 @@
 const bcrypt = require('bcrypt')
 
-const hashPassword = async (pw) => {
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(pw, salt)
-    console.log(salt)
-    console.log(hash)
+// const hashPassword = async (pw) => {
+//     const salt = await bcrypt.genSalt(10);
+//     const hash = await bcrypt.hash(pw, salt)
+//     console.log(salt)
+//     console.log(hash)
+// }
+
+// hashPassword('antone');
+
+const login = async (pw, hashedPw) => {
+    const result = await bcrypt.compare(pw, hashedPw);
+    if(result) {
+        console.log("Bien ahi te logueastee!!")
+    } else {
+        console.log("Mal ahi no te logueaste :(")
+    }
 }
 
-hashPassword('antone');
+login('antone', '$2b$10$GOjKk6s4yVILmCKx0difqOzEv91YFM56MB3o2uSJ.o.Esy4yREfsO');
