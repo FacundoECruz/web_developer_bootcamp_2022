75e68f4874a71282bce94194af4b17c5df72ab7f
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-11-03 15:31:37 -0300

    mongo connection

--- auth_exercises/auth_demo/index.js
+++ auth_exercises/auth_demo/index.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
 const user = require('./models/user')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/authDemo', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected")
+})
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
