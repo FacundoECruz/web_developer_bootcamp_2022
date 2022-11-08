018ada423808d19e2d638bf5d4f27794dae4756d
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-11-08 15:19:43 -0300

    express session added

--- auth_exercises/auth_demo/index.js
+++ auth_exercises/auth_demo/index.js
@@ -3,6 +3,7 @@ const app = express()
 const User = require('./models/user')
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const session = require('express-session')
 
 mongoose.connect('mongodb://localhost:27017/authDemo', {
     useNewUrlParser: true,
@@ -19,6 +20,7 @@ app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: 'notagoodsecret' }))
 
 app.get('/', (req, res) => {
     res.send('This is the home page')
