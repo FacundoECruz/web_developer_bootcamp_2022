003973b283df0c61d87e4269ad3992c34b6fb298
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-11-09 16:32:34 -0300

    register post route setted

--- fulbitoYelp/routes/users.js
+++ fulbitoYelp/routes/users.js
@@ -6,4 +6,13 @@ router.get('/register', (req, res) => {
     res.render('users/register')
 })
 
+router.post('/register', async (req, res) => {
+    const { email, username, password } = req.body;
+    const user = new User({ email, username })
+    const registeredUser = await User.register(user, password)
+    console.log(registeredUser)
+    req.flash('success', 'Bienvenido a Fulbito Yelp!')
+    res.redirect('/soccerfields') 
+})
+
 module.exports = router;
