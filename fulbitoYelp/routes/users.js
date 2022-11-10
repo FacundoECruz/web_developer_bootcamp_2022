3ea1faa185f9516dc8992cecf7ae344251f77ec1
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-11-10 17:15:14 -0300

    automatic login with register

--- fulbitoYelp/routes/users.js
+++ fulbitoYelp/routes/users.js
@@ -9,13 +9,16 @@ router.get('/register', (req, res) => {
     res.render('users/register')
 })
 
-router.post('/register', catchAsync(async (req, res) => {
+router.post('/register', catchAsync(async (req, res, next) => {
     try {
         const { email, username, password } = req.body;
         const user = new User({ email, username })
         const registeredUser = await User.register(user, password)
-        req.flash('success', 'Bienvenido a Fulbito Yelp!')
-        res.redirect('/soccerfields')
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success', 'Bienvenido a Fulbito Yelp!')
+            res.redirect('/soccerfields')
+        })
     } catch (e) {
         req.flash('error', e.message)
         res.redirect('register')
@@ -32,11 +35,11 @@ router.post('/login', passport.authenticate('local', { failureFlash: true, failu
 })
 
 router.get('/logout', (req, res, next) => {
-    req.logout(function(err) {
-      if (err) { return next(err); }
-      req.flash('success', "Goodbye!");
-      res.redirect('/soccerfields');
+    req.logout(function (err) {
+        if (err) { return next(err); }
+        req.flash('success', "Goodbye!");
+        res.redirect('/soccerfields');
     });
-  }); 
+});
 
 module.exports = router;
