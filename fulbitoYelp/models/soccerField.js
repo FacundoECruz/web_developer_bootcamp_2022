542e2b29661183921654dc8fc3313ed318a8f51c
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-11-11 14:55:41 -0300

    author field added to the soccerfield model

--- fulbitoYelp/models/soccerField.js
+++ fulbitoYelp/models/soccerField.js
@@ -8,6 +8,10 @@ const SoccerFieldSchema = new Schema({
     image: String, 
     price: Number,
     description: String,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
     reviews: [
         {
             type: Schema.Types.ObjectId,
