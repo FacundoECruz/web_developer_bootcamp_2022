779197b7498041f37c02c65d51b6c03b8e7ce85b
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-11-19 16:28:11 -0300

    array defined

---
+++ array_methods/index.js
@@ -0,0 +1,27 @@
+const negociosCuadra = [
+    {
+        id: 1,
+        nombre: 'Kiosco Emi',
+        calif: 7,
+        tags: ['Kiosco', 'Lacteos', 'Verduras']
+    },
+    {
+        id: 2,
+        nombre: 'Granja Nahuel',
+        calif: 8,
+        tags: ['Kiosco', 'Lacteos', 'Campo']
+    },
+    {
+        id: 3,
+        nombre: 'Carnica Emi',
+        calif: 6,
+        tags: ['Carnes', 'Bebidas']
+    },
+    {
+        id: 4,
+        nombre: 'Kiosco Media Cuadra',
+        calif: 5,
+        tags: ['Kiosco', 'Bebidas']
+    }
+]
+
