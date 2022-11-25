af6baf375ed156db79a41b8be08cfbc0025b004d
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-11-25 11:11:06 -0300

    cities modified with lat and long

--- fulbitoYelp/seeds/cities.js
+++ fulbitoYelp/seeds/cities.js
@@ -1,29 +1,155 @@
 const provincias = [
-"Ciudad Autónoma de Buenos Aires",
-"Mar del Plata, Provincia de Buenos Aires",
-"Bariloche, Río Negro",
-"Carlos Paz, Córdoba",
-"Villa Gesell, Provincia de Buenos Aires",
-"Termas de Río Hondo, Santiago del Estero",
-"Partido de la Costa, Provincia de Buenos Aires",
-"Salta, Provincia de Salta",
-"Mendoza, Provincia de Mendoza",
-"Iguazú, Misiones",
-"Córdoba, Provincia de Córdoba",
-"Pinamar, Provincia de Buenos Aires",
-"Rosario, Santa Fe",
-"Merlo, San Luis",
-"Colón, Provincia de Entre Ríos",
-"El Calafate, Santa Cruz",
-"Puerto Madryn, Chubut",
-"Tandil, Provincia de Buenos Aires",
-"Ushuaia, Tierra del Fuego",
-"Quebrada de Humahuaca, Jujuy",
-"San Rafael, Mendoza",
-"San Miguel de Tucumán",
-"Federación, Entre Ríos",
-"San Martín de los Andes, Neuquén",
-"Villa General Belgrano, Córdoba",
+    {
+        city: "Ciudad Autónoma de Buenos Aires",
+        state: "Buenos Aires",
+        latitude: -34.5997,
+        longitude: -58.3819
+
+    },
+    {
+        city: "Mar del Plata, Provincia de Buenos Aires",
+        state: "Buenos Aires",
+        latitude: -38.0000,
+        longitude: -57.5500
+    },
+    {
+        city: "Bariloche",
+        state: "Río Negro",
+        latitude: -41.1500,
+        longitude: -71.3000
+    },
+    {
+        city: "Carlos Paz",
+        state: "Córdoba",
+        latitude: -31.4000,
+        longitude: -64.5167
+    },
+    {
+        city: "Villa Gesell",
+        state: "Buenos Aires",
+        latitude: -37.2556,
+        longitude: -56.9681
+    },
+    {
+        city: "Río Grande",
+        state: "Tierra del Fuego",
+        latitude: -53.7914,
+        longitude: -67.6990
+    },
+    {
+        city: "Posadas",
+        state: "Misiones",
+        latitude: -27.3667,
+        longitude: -55.8969
+    },
+    {
+        city: "Salta",
+        state: "Salta",
+        latitude: -24.7883,
+        longitude: -65.4106
+    },
+    {
+        city: "Mendoza",
+        state: "Mendoza",
+        latitude: -32.8833,
+        longitude: -68.8333
+    },
+    {
+        city: "Iguazú",
+        state: "Misiones",
+        latitude: -32.8833,
+        longitude: -68.8333
+    },
+    {
+        city: "Cosquín",
+        state: "Córdoba",
+        latitude: -31.2451,
+        longitude: -64.4656
+    },
+    {
+        city: "Pinamar",
+        state: "Buenos Aires",
+        latitude: -37.1000,
+        longitude: -56.8500
+    },
+    {
+        city: "Rosario",
+        state: "Santa Fe",
+        latitude: -32.9575,
+        longitude: -60.6394
+    },
+    {
+        city: "Merlo",
+        state: "San Luis",
+        latitude: -34.6653,
+        longitude: -58.7275
+    },
+    {
+        city: "Colón",
+        state: "Entre Ríos",
+        latitude: -32.2241,
+        longitude: -58.1419
+    },
+    {
+        city: "El Calafate",
+        state: "Santa Cruz",
+        latitude: -50.3395,
+        longitude: -72.2649
+    },
+    {
+        city: "Puerto Madryn",
+        state: "Chubut",
+        latitude: -42.7730,
+        longitude: -65.0366
+    },
+    {
+        city: "Tandil",
+        state: "Buenos Aires",
+        latitude: -37.3167,
+        longitude: -59.1333
+    },
+    {
+        city: "Ushuaia",
+        state: "Tierra del Fuego",
+        latitude: -54.8072,
+        longitude: -68.3044
+    },
+    {
+        city: "Humahuaca",
+        state: "Jujuy",
+        latitude: -23.2054,
+        longitude: -65.3505
+    },
+    {
+        city: "San Rafael",
+        state: "Mendoza",
+        latitude: -34.6175,
+        longitude: -68.3356
+    },
+    {
+        city: "San Miguel de Tucumán",
+        state: "Tucumán",
+        latitude: -26.8167,
+        longitude: -65.2167
+    },
+    {
+        city: "Federación",
+        state: "Entre Ríos",
+        latitude: -30.9833,
+        longitude: -57.9167
+    },
+    {
+        city: "San Martín de los Andes",
+        state: "Neuquén",
+        latitude: -40.1667,
+        longitude: -71.3500
+    },
+    {
+        city: "Wilde",
+        state: "Buenos Aires",
+        latitude: -34.7000,
+        longitude: -58.3167
+    }
 ]
 
 module.exports = provincias;
