eeb9e1eebc61700cd5658c3415af5630148e84a4
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2022-12-01 22:10:25 -0300

    coep problem solved

--- fulbitoYelp/app.js
+++ fulbitoYelp/app.js
@@ -56,7 +56,55 @@ const sessionConfig = {
 }
 app.use(session(sessionConfig))
 app.use(flash());
-// app.use(helmet({ contentSecurityPolicy: false }));
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+    "https://code.jquery.com/jquery-3.5.1.slim.min.js",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://api.mapbox.com/mapbox-gl-js/v2.11.0/mapbox-gl.js",
+    "https://api.mapbox.com/mapbox-gl-js/v2.11.0/mapbox-gl.css",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://images.unsplash.com/",
+                "https://res.cloudinary.com/dfknsvqer/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    }),
+);
+
 
 app.use(passport.initialize())
 app.use(passport.session())
